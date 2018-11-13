e7edd682ad1528415713cac12c51b4bce409e05a
Eric Zeidan <zeidan@Erics-MacBook-Pro.local>
2018-11-13 13:13:44 +0100

    helper class and use

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/crypto")
      */
     public function indexAction()
     {
