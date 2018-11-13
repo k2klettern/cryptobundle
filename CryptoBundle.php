cb2dcba733e516f0838fe7ab44536b19e7d7ca30
Eric Zeidan <zeidan@Erics-MacBook-Pro.local>
2018-11-13 09:53:55 +0100

    initial commit

---
+++ CryptoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace K2klettern\CryptoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CryptoBundle extends Bundle
+{
+}
---
+++ Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace K2klettern\CryptoBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
